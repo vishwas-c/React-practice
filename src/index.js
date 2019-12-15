7320dbc166a87f500eff7903370e8b47e87e2fb8
Vishwas <vishwasblaze@gmail.com>
2019-12-15 21:24:37 +0530

    Made my own lists

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function MyList() {
+
+function MyInfo() {
   return (
-    <ul><li>no 1</li><li>no 2</li><li>no 3</li></ul>
+    <div>
+      <h1>Vishwas</h1>
+      <p>About myself.Haha</p>
+      <ol>
+        <li>Hawii</li>
+        <li>Bahamas</li>
+        <li>London</li>
+      </ol>
+      <ul>
+        <li>Mahn</li>
+        <li>I'm running out of options</li>
+        <li>Ok. Byeee</li>
+      </ul>
+    </div>
   )
 }
 
-
-ReactDOM.render( <MyList/>, document.getElementById("root"));
+ReactDOM.render(<MyInfo />, document.getElementById('root'))
