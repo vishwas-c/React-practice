714d4a6be0948871be0fa97ddc84821244c3cafe
Vishwas <vishwasblaze@gmail.com>
2019-12-15 21:30:25 +0530

    Moved Component to New file

---
+++ src/MyInfo.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function MyInfo() {
+  return (
+    <div>
+      <h1>Vishwas</h1>
+      <p>About myself.Haha</p>
+      <ol>
+        <li>Hawii</li>
+        <li>Bahamas</li>
+        <li>London</li>
+      </ol>
+      <ul>
+        <li>Mahn</li>
+        <li>I'm running out of options</li>
+        <li>Ok. Byeee</li>
+      </ul>
+    </div>
+  )
+}
+
+export default MyInfo
--- src/index.js
+++ src/index.js
@@ -1,24 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-
-function MyInfo() {
-  return (
-    <div>
-      <h1>Vishwas</h1>
-      <p>About myself.Haha</p>
-      <ol>
-        <li>Hawii</li>
-        <li>Bahamas</li>
-        <li>London</li>
-      </ol>
-      <ul>
-        <li>Mahn</li>
-        <li>I'm running out of options</li>
-        <li>Ok. Byeee</li>
-      </ul>
-    </div>
-  )
-}
+import MyInfo from './MyInfo'
 
 ReactDOM.render(<MyInfo />, document.getElementById('root'))
