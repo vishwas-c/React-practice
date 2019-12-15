982f62d14587dcc28a677ac43bc10a571c343975
Vishwas <vishwasblaze@gmail.com>
2019-12-16 00:25:25 +0530

    Child Component inside Parent component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+      <h4>Hey Mate I'm the footer</h4>
+    </footer>
+  )
+}
+
+export default Footer
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import MyInfo from './components/MyInfo'
+import App from './components/App'
 
-ReactDOM.render(<MyInfo />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
