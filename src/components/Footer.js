59683881800d4a09f02f541ea02977b7c3499c53
Vishwas <vishwasblaze@gmail.com>
2019-12-19 16:38:02 +0530

    Added Filters

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import Joke from './Joke'
+import jokesData from "./jokesData"
 
 function Footer() {
+  const JokeComponents = jokesData
+  .filter( filteredjoke => filteredjoke.question)
+  .map(joke => 
+    <Joke key={joke.id} question={joke.question} punchline={joke.punchLine} /> 
+  )
+  
   return (
-    <footer>
-      <h4 className="onlyme">Hey Mate I'm the footer</h4>
-    </footer>
+    <div>
+      {JokeComponents}
+      <footer>
+        <h4 className="onlyme">Hey Mate I'm the footer</h4>
+      </footer>
+    </div>
   )
 }
 
